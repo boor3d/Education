33edd3e31f715a4622dc333cb906ebc868ad8978
boo_r3d <jest3r.boor3d@gmail.com>
2025-01-07 15:26:31 -0500

    new test

--- PyTesting/tests/test_watches.py
+++ PyTesting/tests/test_watches.py
@@ -11,3 +11,8 @@ def test_create_person_NoneType():
     watch_collection = watchCollection(name=None)
     assert watch_collection.name == "None"
     assert type(watch_collection.watches) == dict
+
+def test_create_person_num():
+    watch_collection = watchCollection(name=1010)
+    assert watch_collection.name == "1010"
+    assert type(watch_collection.watches) == dict
