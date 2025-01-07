add366fa8c97f6de632a39ec6a6f9e583078fca5
boo_r3d <jest3r.boor3d@gmail.com>
2025-01-07 15:28:07 -0500

    intent to fail function in test

--- PyTesting/tests/test_watches.py
+++ PyTesting/tests/test_watches.py
@@ -16,3 +16,9 @@ def test_create_person_num():
     watch_collection = watchCollection(name=1010)
     assert watch_collection.name == "1010"
     assert type(watch_collection.watches) == dict
+
+
+def test_create_person_num():
+    watch_collection = watchCollection(name=1010)
+    assert watch_collection.name == "Intent to Fail"
+    assert type(watch_collection.watches) == dict
